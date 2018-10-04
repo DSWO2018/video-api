04fab341cea235f8292013f8047ce407ed9ba669
Hiram Torres <is700845@iteso.mx>
2018-10-04 16:46:11 -0500

    Updated demo

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,15 @@
 package controllers
 
 import javax.inject._
-
 import play.api.mvc._
+import play.api.libs.json._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
+class HomeController @Inject()(cc: ControllerComponents)
   extends AbstractController(cc) {
 
   /**
@@ -19,7 +19,7 @@ class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder:
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+      Ok(Json.obj("status" ->"OK", "message" -> "Hello world!"))
   }
 
 }
