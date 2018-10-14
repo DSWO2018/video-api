6d6d818f29a1ff57faa5d0e31aefbd57f93afa45
Hiram Torres <is700845@iteso.mx>
2018-10-14 17:13:50 -0500

    Add unit test

--- test/UserSpec.scala
+++ test/UserSpec.scala
@@ -7,9 +7,10 @@ import play.api.libs.json.Json
 class UserSpec extends PlaySpec with MockitoSugar with ScalaFutures {
   "User" should {
     "create a user object from JSON" in {
-      val data = Json.obj("email" ->"is700845@iteso.mx", "password" -> "UltraSecretPassword")
+      val data = Json.obj("email" ->"is700845@iteso.mx", "password" -> "UltraSecretPassword",
+        "first_name" -> "Hiram", "last_name" -> "Torres", "description" -> "Sample.")
       val user = data.validate[User]
-      user.get.mustBe(User(None, "is700845@iteso.mx", "UltraSecretPassword"))
+      user.get.mustBe(User(None, "is700845@iteso.mx", "UltraSecretPassword", "Hiram", "Torres", "Sample."))
     }
     "reject a JSON with invalid email or short password" in {
       val data = Json.obj("email" ->"is700845itesomx", "password" -> "123456")
